611145aca4da57cadeb23aa841d2513cb3a31d7f
NathanOliveiraJF <58376382+NathanOliveiraJF@users.noreply.github.com>
2021-03-31 18:23:34 -0300

    add models

---
+++ backend/Album/AlbumDomain/Models/Album.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlbumDomain.Models
+{
+    public class Album
+    {
+        public string AlbumId { get; set; }
+        public string Nome { get; set; }
+        public DateTime AnoLancamento { get; set; }
+        public IList<Musica> Musicas { get; set; } = new List<Musica>();
+    }
+}
---
+++ backend/Album/AlbumDomain/Models/Musica.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlbumDomain.Models
+{
+    public class Musica
+    {
+        public string MusicaId { get; private set; }
+        public string AlbumId { get; private set; }
+        public string Nome { get; set; }
+        public double Duracao { get; set; }
+    }
+}
