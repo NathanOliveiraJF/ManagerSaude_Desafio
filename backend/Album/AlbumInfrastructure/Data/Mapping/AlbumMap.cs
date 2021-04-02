982fcf8d54b19010e082558d97f784f3a2f0bdf6
NathanOliveiraJF <58376382+NathanOliveiraJF@users.noreply.github.com>
2021-04-01 22:20:00 -0300

    add mapeamento entidades

---
+++ backend/Album/AlbumInfrastructure/Data/Mapping/AlbumMap.cs
@@ -0,0 +1,31 @@
+﻿using AlbumDomain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlbumInfrastructure.Data.Mapping
+{
+    public class AlbumMap : IEntityTypeConfiguration<Album>
+    {
+        public void Configure(EntityTypeBuilder<Album> builder)
+        {
+            builder.Property(x => x.AlbumId)
+                .HasDefaultValueSql("NEWID()");
+
+            builder.HasMany(x => x.Musicas)
+                .WithOne()
+                .HasForeignKey(x => x.AlbumId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Property(x => x.Nome)
+                .HasColumnType("nvarchar(50)")
+                .IsRequired();
+
+            builder.Property(x => x.AnoLancamento)
+                .HasColumnType("datetime2")
+                .IsRequired();
+        }
+    }
+}
---
+++ backend/Album/AlbumInfrastructure/Data/Mapping/MusicaMap.cs
@@ -0,0 +1,28 @@
+﻿using AlbumDomain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlbumInfrastructure.Data.Mapping
+{
+    public class MusicaMap : IEntityTypeConfiguration<Musica>
+    {
+        public void Configure(EntityTypeBuilder<Musica> builder)
+        {
+            builder.Property(x => x.MusicaId)
+                .HasDefaultValueSql("NEWID()");
+
+            builder.Property(x => x.AlbumId)
+                .IsRequired();
+
+            builder.Property(x => x.Duracao)
+                .IsRequired();
+
+            builder.Property(x => x.Nome)
+                .HasColumnType("nvarchar(50)")
+                .IsRequired();
+        }
+    }
+}
