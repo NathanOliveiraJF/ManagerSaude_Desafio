fcdef30d0fcb756d0794b1a494fa4fd1732504da
NathanOliveiraJF <58376382+NathanOliveiraJF@users.noreply.github.com>
2021-04-01 22:20:24 -0300

    add context

---
+++ backend/Album/AlbumInfrastructure/Data/Context/AlbumApplicationContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AlbumDomain.Models;
+using AlbumInfrastructure.Data.Mapping;
+
+namespace AlbumInfrastructure.Data.Context
+{
+    public class AlbumApplicationContext : DbContext
+    {
+        public AlbumApplicationContext (DbContextOptions<AlbumApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Album> Album { get; set; }
+        public DbSet<Musica> Musica { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new AlbumMap());
+            modelBuilder.ApplyConfiguration(new MusicaMap());
+        }
+    }
+}
