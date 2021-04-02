5bc3666df89d521aa6842e451c1e570ba795ca5c
NathanOliveiraJF <58376382+NathanOliveiraJF@users.noreply.github.com>
2021-04-01 22:21:13 -0300

    add migrations

---
+++ backend/Album/AlbumInfrastructure/Migrations/20210401193222_Initial.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AlbumInfrastructure.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Album",
+                columns: table => new
+                {
+                    AlbumId = table.Column<string>(type: "nvarchar(450)", nullable: false, defaultValueSql: "NEWID()"),
+                    Nome = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    AnoLancamento = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Album", x => x.AlbumId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Musica",
+                columns: table => new
+                {
+                    MusicaId = table.Column<string>(type: "nvarchar(450)", nullable: false, defaultValueSql: "NEWID()"),
+                    AlbumId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(50)", nullable: false),
+                    Duracao = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Musica", x => x.MusicaId);
+                    table.ForeignKey(
+                        name: "FK_Musica_Album_AlbumId",
+                        column: x => x.AlbumId,
+                        principalTable: "Album",
+                        principalColumn: "AlbumId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Musica_AlbumId",
+                table: "Musica",
+                column: "AlbumId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Musica");
+
+            migrationBuilder.DropTable(
+                name: "Album");
+        }
+    }
+}
